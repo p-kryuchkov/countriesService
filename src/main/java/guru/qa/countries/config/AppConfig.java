fb29e63b0f14ff5b67c34f25f0c35e5e12eed024
Pavel Kryuchkov <krutestkru@gmail.com>
2026-02-03 22:42:59 +0300

    Замечания по ПР

--- src/main/java/guru/qa/countries/config/AppConfig.java
+++ src/main/java/guru/qa/countries/config/AppConfig.java
@@ -28,12 +28,12 @@ public class AppConfig {
         return new ServletRegistrationBean<>(servlet, "/ws/*");
     }
     @Bean(name = "countries")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema userdataSchema) {
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
         final DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("CountriesPort");
         wsdl11Definition.setLocationUri(countriesBaseUri + "/ws");
         wsdl11Definition.setTargetNamespace(SOAP_NAMESPACE);
-        wsdl11Definition.setSchema(userdataSchema);
+        wsdl11Definition.setSchema(countriesSchema);
         return wsdl11Definition;
     }
 
